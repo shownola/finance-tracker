33c81bc2ea8f5bb4a3021f7958898d29c7231db1
Rails Udemy Course <swasieko@gmail.com>
2016-12-09 04:09:07 +0000

    Add stocks and ajax for UI

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   
  root 'welcome#index'
+ get 'my_portfolio', to: "users#my_portfolio"
+ get 'search_stocks', to: "stocks#search"
  
 #  get 'welcome/home', to: 'welcome#home'
 
